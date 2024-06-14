2750a2d525bf6fc09f9e68001d9dfe9db654505a
gabassis <gabriel.assis2309@gmail.com>
2024-06-13 21:12:16 -0300

    First Commit

---
+++ src/main/java/com/example/model/ClienteModel.java
@@ -0,0 +1,77 @@
+package com.example.model;
+
+import javax.persistence.GenerationType;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tb_clientes")
+public class ClienteModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "telefone", nullable = true)
+    private String telefone;
+
+    @Column(name = "documento", nullable = false)
+    private String documento;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    @Override
+    public String toString() {
+        return "ClienteModel{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", telefone='" + telefone + '\'' +
+                ", documento='" + documento + '\'' +
+                '}';
+    }
+}
